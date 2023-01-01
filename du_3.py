ccec1c3931b6a7727aeaa798f859452f855c6540
Barbora Paulusová <paulusob@natur.cuni.cz>
2023-01-01 17:19:15 +0100

    převedení seznamu adres na slovník

--- du_3.py
+++ du_3.py
@@ -4,21 +4,29 @@ with open ("adresy.geojson", encoding="utf-8") as f:
     data_a=json.load(f)
 
 adresy=data_a['features']
+adresy_l=list(adresy)
+#for y in adresy_l:
+    #print(y)
 #ulice=data['addr:street']
 #print (adresy)
+#print (adresy_l)
 
+adresy_sez={}
 
-adresy_sez=[]
-for item in adresy:
-    adresa=item ["properties"]
-    ulice=adresa["addr:street"]
-    cislo=adresa["addr:housenumber"]
-    adresy_sez.append(ulice)
-    adresy_sez.append(cislo)
-    poloha_a=item["geometry"]
-    souradnice_a=poloha_a["coordinates"]
-    adresy_sez.append(souradnice_a)
+for item in adresy_l:
+    adresa=item ['properties']
+    id_a=adresa['@id']
+    ulice=adresa['addr:street']
+    cislo=adresa['addr:housenumber']
+    poloha_a=item['geometry']
+    souradnice_a=poloha_a['coordinates']
+    seznam=[ulice, cislo, souradnice_a]
+    adresy_sez[id_a]=seznam
+    
+    
 print (adresy_sez)
+
+
 
 with open ("kontejnery.geojson", encoding="utf-8") as f: 
     data_k=json.load(f)
@@ -29,6 +37,6 @@ for item in kontejnery:
     poloha_k=item['geometry']
     souradnice_k=poloha_k['coordinates']
     kontejnery_sez.append(souradnice_k)
-print(kontejnery_sez)
+#print(kontejnery_sez)
 
 
