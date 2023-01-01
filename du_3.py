b6123bec6627f2c30be5c29823bcbe7c4ee75e47
Barbora Paulusová <paulusob@natur.cuni.cz>
2023-01-01 17:32:04 +0100

    převedení seznamu kontejnerů na slovník

--- du_3.py
+++ du_3.py
@@ -5,11 +5,7 @@ with open ("adresy.geojson", encoding="utf-8") as f:
 
 adresy=data_a['features']
 adresy_l=list(adresy)
-#for y in adresy_l:
-    #print(y)
-#ulice=data['addr:street']
-#print (adresy)
-#print (adresy_l)
+
 
 adresy_sez={}
 
@@ -20,23 +16,34 @@ for item in adresy_l:
     cislo=adresa['addr:housenumber']
     poloha_a=item['geometry']
     souradnice_a=poloha_a['coordinates']
-    seznam=[ulice, cislo, souradnice_a]
-    adresy_sez[id_a]=seznam
+    seznam_a=[ulice, cislo, souradnice_a]
+    adresy_sez[id_a]=seznam_a
     
     
 print (adresy_sez)
-
 
 
 with open ("kontejnery.geojson", encoding="utf-8") as f: 
     data_k=json.load(f)
 
 kontejnery=data_k['features']
-kontejnery_sez=[]
-for item in kontejnery:
+kontejnery_l=list(kontejnery)
+
+kontejnery_sez={}
+
+for item in kontejnery_l:
+    
     poloha_k=item['geometry']
     souradnice_k=poloha_k['coordinates']
-    kontejnery_sez.append(souradnice_k)
-#print(kontejnery_sez)
+    vlastnosti=item ['properties']
+    id_k=vlastnosti['ID']
+    pristup=vlastnosti['PRISTUP']
+    seznam_k=[souradnice_k, pristup]
+    if "volně" in pristup:
+        kontejnery_sez[id_k]=seznam_k
+    else:
+        continue
+    
+print(kontejnery_sez)
 
 
